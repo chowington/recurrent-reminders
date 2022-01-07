2d8b10e404636460068c17d572a2ed63b8dfbcb4
Connor Howington <connor.howington@gmail.com>
2022-01-07 11:33:10 -0500

    Basic app changes

--- App.tsx
+++ App.tsx
@@ -1,18 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Duration } from 'luxon';
+
+interface Reminder {
+  title: string;
+  interval: Duration;
+  lastCompletion?: Date;
+}
 
 export default function App() {
+  const [reminders, setReminders] = useState<Array<Reminder>>([
+    {
+      title: 'Play guitar',
+      interval: Duration.fromObject({weeks: 1}),
+    },
+    {
+      title: 'Read a book',
+      interval: Duration.fromObject({days: 3}),
+    },
+    {
+      title: 'Exercise',
+      interval: Duration.fromObject({days: 2}),
+    },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{marginTop: 30, padding: 20}}>
+      <FlatList
+        data={reminders}
+        renderItem={({item: reminder, index}) => (
+          <View key={index}>
+            <Text>
+              {reminder.title + ' interval: ' + reminder.interval.as('days') + ' days'}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
