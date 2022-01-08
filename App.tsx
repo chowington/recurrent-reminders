b958bb011c6bdd0e0cf5a8a427f287e14efefb58
Connor Howington <connor.howington@gmail.com>
2022-01-08 01:04:47 -0500

    Use async storage to store reminders

--- App.tsx
+++ App.tsx
@@ -1,41 +1,85 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, View, FlatList } from 'react-native';
 import { Duration } from 'luxon';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+interface DurationObject {
+  seconds?: number;
+  minutes?: number;
+  hours?: number;
+  days?: number;
+  weeks?: number;
+  months?: number;
+  years?: number;
+}
 
 interface Reminder {
   title: string;
-  interval: Duration;
+  interval: DurationObject;
   lastCompletion?: Date;
 }
 
+const storeData = async (key: string, value: any) => {
+  try {
+    const jsonValue = JSON.stringify(value)
+    await AsyncStorage.setItem(key, jsonValue)
+  } catch (e) {
+    // saving error
+  }
+}
+
+const getData = async (key: string) => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key)
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch(e) {
+    // error reading value
+  }
+}
+
+const initialReminders: Reminder[] = [
+  {
+    title: 'Play guitar',
+    interval: {weeks: 1},
+  },
+  {
+    title: 'Read a book',
+    interval: {days: 3},
+  },
+  {
+    title: 'Exercise',
+    interval: {days: 2},
+  },
+];
+
 export default function App() {
-  const [reminders, setReminders] = useState<Array<Reminder>>([
-    {
-      title: 'Play guitar',
-      interval: Duration.fromObject({weeks: 1}),
-    },
-    {
-      title: 'Read a book',
-      interval: Duration.fromObject({days: 3}),
-    },
-    {
-      title: 'Exercise',
-      interval: Duration.fromObject({days: 2}),
-    },
-  ]);
+  const [reminders, setReminders] = useState<Array<Reminder>>([]);
+
+  useEffect(() => {
+    const getReminders = async () => {
+      // await storeData('reminders', initialReminders);
+      const storedReminders = await getData('reminders');
+      // console.log(storedReminders);
+      setReminders(storedReminders);
+    }
+
+    getReminders();
+  });
 
   return (
-    <View style={{marginTop: 30, padding: 20}}>
-      <FlatList
-        data={reminders}
-        renderItem={({item: reminder, index}) => (
-          <View key={index}>
-            <Text>
-              {reminder.title + ' interval: ' + reminder.interval.as('days') + ' days'}
-            </Text>
-          </View>
-        )}
-      />
+    <View style={{width: '100%', height: '100%', backgroundColor: 'black'}}>
+      <View style={{padding: 20}}>
+        <FlatList
+          data={reminders}
+          renderItem={({item: reminder, index}) => (
+            <View key={index}>
+              <Text style={{color: 'white'}}>
+                {reminder.title + ' -- interval: ' + Duration.fromObject(reminder.interval).as('days') + ' days'}
+              </Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
