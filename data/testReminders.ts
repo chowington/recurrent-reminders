348e64b18d2489a37304c9f0cd819477df8c8357
Connor Howington <connor.howington@gmail.com>
2022-01-23 15:44:05 -0500

    Make testReminders more flexible

--- data/testReminders.ts
+++ data/testReminders.ts
@@ -1,22 +1,27 @@
 import { ReminderProps } from '../components/Reminder';
+import { DateTime } from 'luxon';
+
+const daysAgo = (days: number) =>
+  DateTime.now().startOf('day').minus({ days: days }).toISODate();
 
 export const testReminders: ReminderProps[] = [
   {
     id: '83ca3c76-8bd1-42ae-86e7-60a8149b6a0e',
     title: 'Do great work',
-    startDate: '2022-01-02',
+    startDate: daysAgo(30),
     interval: {
       days: 1,
     },
+    lastCompletion: daysAgo(11),
   },
   {
     id: '4fcbd93e-b9a5-438a-99fb-01b4e0f513cb',
     title: 'Exercise',
-    startDate: '2022-01-12',
+    startDate: daysAgo(30),
     interval: {
       days: 2,
     },
-    lastCompletion: '2022-01-14',
+    lastCompletion: daysAgo(5),
   },
   {
     id: 'd9f9eb38-3374-482a-a3e7-72099377d8e8',
@@ -24,8 +29,8 @@ export const testReminders: ReminderProps[] = [
     interval: {
       days: 2,
     },
-    startDate: '2022-12-11',
-    lastCompletion: '2021-12-20',
+    startDate: daysAgo(12),
+    lastCompletion: daysAgo(1),
   },
   {
     id: '6c8d5173-76ad-44cb-b3c7-d854c05450c6',
@@ -33,13 +38,13 @@ export const testReminders: ReminderProps[] = [
     interval: {
       days: 3,
     },
-    startDate: '2021-12-22',
-    lastCompletion: '2022-01-20',
+    startDate: daysAgo(56),
+    lastCompletion: daysAgo(8),
   },
   {
     id: '87202216-c555-4d0e-80fb-0eb90256dfb5',
     title: 'Play guitar',
-    startDate: '2022-01-15',
+    startDate: daysAgo(0),
     interval: {
       days: 7,
     },
@@ -47,11 +52,11 @@ export const testReminders: ReminderProps[] = [
   {
     id: 'bff17177-b5be-4ba0-803b-b16ad76999fa',
     title: 'Work on app',
-    startDate: '2022-01-11',
+    startDate: daysAgo(8),
     interval: {
       days: 2,
     },
-    lastCompletion: '2022-01-15',
+    lastCompletion: daysAgo(0),
   },
   {
     id: '7150dae7-5dc8-4c2f-ad50-932fdeb569f5',
@@ -59,8 +64,8 @@ export const testReminders: ReminderProps[] = [
     interval: {
       days: 28,
     },
-    startDate: '2022-01-01',
-    lastCompletion: '2022-01-01',
+    startDate: daysAgo(60),
+    lastCompletion: daysAgo(3),
   },
   {
     id: '8a70b960-5b8f-4b67-9c23-7559884ef230',
@@ -68,7 +73,7 @@ export const testReminders: ReminderProps[] = [
     interval: {
       days: 7,
     },
-    startDate: '2022-01-05',
-    lastCompletion: '2022-01-17',
+    startDate: daysAgo(22),
+    lastCompletion: daysAgo(2),
   },
 ];
