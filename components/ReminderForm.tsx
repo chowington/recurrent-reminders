8947e63205a0a6e0d194c2b3cf0580de8898799f
Connor Howington <connor.howington@gmail.com>
2022-01-30 01:14:25 -0500

    Allow modifying last completion date

--- components/ReminderForm.tsx
+++ components/ReminderForm.tsx
@@ -23,31 +23,45 @@ export default function ReminderForm(props: ReminderFormProps) {
       ? DateTime.fromISO(props.reminder.startDate).toJSDate()
       : new Date(Date.now())
   );
+  const [lastCompletion, setLastCompletion] = useState<Date | undefined>(
+    props.reminder?.lastCompletion
+      ? DateTime.fromISO(props.reminder.lastCompletion).toJSDate()
+      : undefined
+  );
+
   const [datePickerIsVisible, setDatePickerIsVisible] = useState(false);
+  const [dateInFocus, setDateInFocus] = useState<
+    'startDate' | 'lastCompletion'
+  >('startDate');
+
   const { addReminder, updateReminder } = useContext(RemindersContext);
 
   const isValid = taskName.length > 0 && interval > 0;
 
   const onDismissSingle = useCallback(() => {
+    if (dateInFocus === 'lastCompletion') setLastCompletion(undefined);
     setDatePickerIsVisible(false);
-  }, [setDatePickerIsVisible]);
+  }, [dateInFocus, setDatePickerIsVisible]);
 
   const onConfirmSingle = useCallback(
     (params) => {
+      if (dateInFocus === 'startDate') setStartDate(params.date);
+      else setLastCompletion(params.date);
       setDatePickerIsVisible(false);
-      setStartDate(params.date);
     },
-    [setDatePickerIsVisible, setStartDate]
+    [dateInFocus]
   );
 
   const onComplete = useCallback(() => {
-    const newReminder = {
+    const newReminder: Omit<ReminderProps, 'id'> = {
       title: taskName,
       interval: { days: interval },
       startDate: DateTime.fromJSDate(startDate).toISODate(),
     };
 
     if (props.reminder) {
+      newReminder.lastCompletion =
+        lastCompletion && DateTime.fromJSDate(lastCompletion).toISODate();
       updateReminder(props.reminder.id, newReminder);
     } else {
       addReminder(newReminder);
@@ -61,6 +75,7 @@ export default function ReminderForm(props: ReminderFormProps) {
     taskName,
     interval,
     startDate,
+    lastCompletion,
     props.navigation,
   ]);
 
@@ -104,7 +119,12 @@ export default function ReminderForm(props: ReminderFormProps) {
       <Text style={{ color: 'white', fontSize: 26, marginBottom: 10 }}>
         Start date
       </Text>
-      <Pressable onPress={() => setDatePickerIsVisible(true)}>
+      <Pressable
+        onPress={() => {
+          setDateInFocus('startDate');
+          setDatePickerIsVisible(true);
+        }}
+      >
         <Text
           style={{
             color: 'white',
@@ -121,12 +141,41 @@ export default function ReminderForm(props: ReminderFormProps) {
           {startDate.toLocaleDateString()}
         </Text>
       </Pressable>
+      {props.reminder && (
+        <>
+          <Text style={{ color: 'white', fontSize: 26, marginBottom: 10 }}>
+            Last completion date
+          </Text>
+          <Pressable
+            onPress={() => {
+              setDateInFocus('lastCompletion');
+              setDatePickerIsVisible(true);
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                borderStyle: 'solid',
+                borderColor: 'lightblue',
+                borderWidth: 2,
+                borderRadius: 10,
+                height: 50,
+                padding: 10,
+                marginBottom: 30,
+                textAlignVertical: 'center',
+              }}
+            >
+              {lastCompletion && lastCompletion.toLocaleDateString()}
+            </Text>
+          </Pressable>
+        </>
+      )}
       <DatePickerModal
         locale='en'
         mode='single'
         visible={datePickerIsVisible}
         onDismiss={onDismissSingle}
-        date={startDate}
+        date={dateInFocus === 'startDate' ? startDate : lastCompletion}
         onConfirm={onConfirmSingle}
         saveLabel='Select'
         uppercase={false}
